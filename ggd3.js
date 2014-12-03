7a5194e3a62940fdbfe890306b8aa1ee031c86c6
jtschoonhoven <jtschoonhoven@gmail.com>
2014-12-03 09:40:36 -0800

    minor additions

--- ggd3.js
+++ ggd3.js
@@ -6,7 +6,7 @@
 
 (function () {
 
-  // Configure for Node and the browser.
+  // Configure for Node and the browser
   // ----------------------------------------------------
   // GGD3 needs a DOM in order to build an SVG. If used
   // outside of the browser, the DOM is stubbed by jsdom.
@@ -15,39 +15,45 @@
 
   if (typeof module !== 'undefined' && module.exports) {
     isNode = true;
+
     document = require('jsdom').jsdom();
     window = document.parentWindow;
+
     this.d3 = require('d3');
     this._  = require('underscore');
+
   }
 
   if (!d3 || !_) { throw 'ggd3 requires D3 and Underscore.'; }
 
-  // Declare the module and top level methods.
+  // Declare the module and top level methods
   // ----------------------------------------------------
   // GGD3 starts as a humble object literal. It contains
   // all methods used to create a new Graphic and will
   // be exported to the global object.
 
-  var ggd3 = {};
+  var ggd3 = {
+    configure: function(opts) {},
+    data: function(data) {}
+  };
 
   // "Create" is a one-step method to create a new
-  // graphic. Internally it simply calls the four other
-  // top level methods in order.
-  // ----------------------------------------------------
+  // graphic. Internally it simply calls all (or some) of 
+  // the other top level methods in order.
 
   var create = ggd3.create = function(opts) {
-    if (opts.opts)   { this.options(opts); }
+    if (opts.opts)   { this.configure(opts); }
     if (opts.data)   { this.data(data); }
     if (opts.el)     { this.draw(el, height, width); }
     if (opts.render) { this.render(); }
   };
 
-
-  var Graphic = ggd3.Graphic = function() {}
-
-  _.extend(Graphic.prototype, {});
-
+  // Configure
+  // ----------------------------------------------------
+  // "Configure" is a top level method that configures
+  // the component controllers. Controllers are
+  // primarily factories for components, though they
+  // contain other properties as well.
 
   var configure = ggd3.configure = function(opts) {
 
@@ -64,20 +70,15 @@
     initialize: function() {}
   });
 
+  // Export global
+  // ----------------------------------------------------
+  // Export as a Node module if applicable. Either way,
+  // attach ggd3 to the global object.
 
+  if (isNode) {
+    module.exports = ggd3;
+  }
 
-  // =============
-  // EXPORT GLOBAL
-  // =============
-
-
-  // Export as Node module.
-  // if (isNode) {
-    // module.exports = Graphic;
-  // }
-
-  // Export to global context.
-  // this.Graphic = Graphic;
-
+  this.ggd3 = ggd3;
 
 })();
