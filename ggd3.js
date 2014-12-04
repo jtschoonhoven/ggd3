f6a81e193c6de3af6ab5da12c0958966496e5748
jtschoonhoven <jtschoonhoven@gmail.com>
2014-12-03 17:56:10 -0800

    working on factories

--- ggd3.js
+++ ggd3.js
@@ -6,6 +6,8 @@
 
 (function () {
 
+
+
   // Configure for Node and the browser
   // ----------------------------------------------------
   // GGD3 needs a DOM in order to build an SVG. If used
@@ -26,6 +28,8 @@
 
   if (!d3 || !_) { throw 'ggd3 requires D3 and Underscore.'; }
 
+
+
   // Declare the module and top level methods
   // ----------------------------------------------------
   // GGD3 starts as a humble object literal. It contains
@@ -33,11 +37,14 @@
   // be exported to the global object.
 
   var ggd3 = {};
-  ggd3.VERSION = 0.0.0
+
+  ggd3.VERSION = '0.0.0';
+
+
 
   // "Create" returns a new instance of Graphic and
   // optionally configures and renders it to the DOM.
-  // None of the arguments are required. 
+  // All arguments are optional and can be set later. 
 
   ggd3.create = function(opts, data, selector, width, height, renderNow) {
     var graphic = new Graphic();
@@ -48,6 +55,8 @@
     return graphic;
   };
 
+
+
   // The Graphic constructor
   // ----------------------------------------------------
   // A new Graphic contains all the methods that 
@@ -55,7 +64,9 @@
   // represented by four functions. In order,
   // **configure**, **data**, **draw**, and **render**.
 
-  var Graphic = function() {}
+  var Graphic = function() {};
+
+
 
   // Step #1: Configure
   // ----------------------------------------------------
@@ -66,7 +77,89 @@
   // that, when data is applied to the factory function,
   // a properly configured component is returned.
 
-  Graphic.prototype.configure = function(opts) {};
+  // The following options may be passed as opts to
+  // graphic.configure. 
+
+  var optionsDefaults = {
+    flow: undefined,
+    gridX: undefined,
+    gridY: undefined,
+    x: undefined,
+    y: undefined,
+    color: undefined,
+    size: undefined,
+    group: undefined,
+    geometry: 'point',
+    scaleX: 'categorical',
+    scaleY: 'linear',
+    coordinates: 'cartesian',
+    facets: {},
+    layers: []
+  };
+
+
+
+  // Components.
+
+  var gridFacet = {};
+  var flowFacet = {};
+  var layer = {};
+
+
+
+  // "ComponentFactory" is a generic factory that will be
+  // used to extend new component factories.
+
+  var ComponentFactory = function() {
+    var that = this;
+    this.Constructor = function(properties) { _.extend(this, properties); };
+    this.create = function(properties) { return new that.Constructor(properties); };
+  };
+
+
+  // "FacetFactory" extends ComponentFactory and then
+  // creates an array of LayerFactories for each layer
+  // in opts (at least one). If the user has specified
+  // grid faceting then extend from GridFacet.
+  // Otherwise extend from FlowFacet.
+
+  var FacetFactory = function(opts) {
+    this.opts = _.extend(opts, opts.facets);
+    ComponentFactory.apply(this);
+
+    opts.layers[0] = opts.layers[0] || {};
+    this.layerFactories = opts.layers.map(function(layer, index) {
+      return new LayerFactory(opts, index);
+    });
+
+    if (opts.gridX || opts.gridY) { this.data = gridFacet.data; }
+    else { this.data = flowFacet.data; }
+  };
+
+
+
+  var LayerFactory = function(opts, index) {
+    this.opts = _.extend(opts, opts.facets[index]);
+    ComponentFactory.apply(this);
+    _.extend(this.Constructor.prototype, new Layer());
+    this.geometryFactory = new GeometryFactory(opts, index);
+  };
+
+
+
+  var GeometryFactory = function(opts, index) {
+    this.opts = _.extend(opts, opts.facets[index]);
+    ComponentFactory.apply(this);
+  };
+
+
+
+  Graphic.prototype.configure = function(opts) {
+    this.opts = _.defaults(opts || {}, optionsDefaults);
+    this.facetFactory = new FacetFactory(opts);
+  };
+
+
 
   // Step #2: Apply data
   // ----------------------------------------------------
@@ -74,7 +167,17 @@
   // configured, the next step is to apply that data to
   // the factories and manufacture some components.
 
-  Graphic.prototype.data = function(data) {};
+  Graphic.prototype.data = function(dataset) {
+    this.facets = this.FacetFactory.data(dataset);
+  };
+
+
+
+  flowFacets.data = function(dataset) {
+    var that = this;
+  };
+
+
 
   // Step #3: Draw SVG
   // ----------------------------------------------------
@@ -83,6 +186,8 @@
   // attached to the DOM. That is accomplished in step 4.
 
   Graphic.prototype.draw = function(selector, height, width) {};
+
+
 
   // Step #4: Render SVG
   // ----------------------------------------------------
@@ -93,23 +198,13 @@
     this.target.html(this.el.html());
   };
 
+
+
   // Return SVG HTML or undefined.
 
   Graphic.prototype.html = function() {};
 
-  // "Extend" should look familiar if you've used 
-  // Backbone. Returns a new object that inherits from
-  // its parent and defined properties.
 
-  var extend = function(protoProperties, instanceProperties) {
-    var parent = this;
-    var child = parent.apply(this, arguments);
-    _.extend(child, parent, instanceProperties);
-    child.prototype = parent.prototype;
-    return child;
-  };
-
-  Factory.extend = extend;
 
   // Export global
   // ----------------------------------------------------
@@ -118,5 +213,7 @@
 
   if (isNode) { module.exports = ggd3; }
   this.ggd3 = ggd3;
+
+
 
 })();
